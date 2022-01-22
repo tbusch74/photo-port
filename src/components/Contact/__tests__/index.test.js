e94af37e1755806845b10b76b2b28005fec801d1
Tony <abusch@css.edu>
2022-01-22 01:42:51 -0600

    lesson 5

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import Contact from './Contact'
+import Contact from '..'
 
 afterEach(cleanup)
 
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -33,7 +33,7 @@ function ContactForm() {
       
     return (
         <section>
-            <h1>Contact me</h1>
+            <h1 data-testid="h1tag">Contact me</h1>
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
@@ -52,7 +52,7 @@ function ContactForm() {
                     <p className="error-text">{errorMessage}</p>
                 </div>
                 )}
-                <button type="submit" onSubmit={handleSubmit}>Submit</button>
+                <button type="submit" data-testid="button" onSubmit={handleSubmit}>Submit</button>
             </form>
         </section>
     )
